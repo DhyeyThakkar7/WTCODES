ee1464df93c2ed65fd8f0cdc879a494c10be0512
DhyeyThakkar7 <dhyey576@gmail.com>
2024-11-25 06:02:54 +0530

    Create script.js

---
+++ Design and develop a responsive website to calculate Electricity bill using Node JS Condition for first 50 units – Rs. 3.50 unit, for next 100 units – Rs. 4.00 unit, for next 100 units – Rs. 5.20 unit and for units above 250 – Rs. 6.50 unit. You can make the use of bootstrap as well as jQuery. /script.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    $('#calculate').click(function() {
+        const units = parseFloat($('#units').val());
+        let bill = 0;
+
+        if (units <= 50) {
+            bill = units * 3.50;
+        } else if (units <= 150) {
+            bill = (50 * 3.50) + ((units - 50) * 4.00);
+        } else if (units <= 250) {
+            bill = (50 * 3.50) + (100 * 4.00) + ((units - 150) * 5.20);
+        } else {
+            bill = (50 * 3.50) + (100 * 4.00) + (100 * 5.20) + ((units - 250) * 6.50);
+        }
+
+        $('#result').text(Total Electricity Bill: Rs. ${bill.toFixed(2)});
+    });
+});
